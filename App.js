39c7cc8b635428d8bc3c1559a90ee52b5cd19088
Meenakshi <meenakshi.learner@gmail.com>
2024-08-09 02:34:12 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
-const heading = React.createElement("h1", {id:"heading"}, "Hello World From React!")
-const root = ReactDOM.createRoot(document.getElementById("root"))
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World From React!"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading)
-const head = document.getElementById("root")
-const createHead = document.createElement("h2")
-console.log(head)
-console.log(createHead)
-console.log(heading)
-const parent = React.createElement("div", {id: "parentDiv"}, 
-React.createElement("div", {id:"childDiv"},
-[React.createElement("h1", {id:"h1Tag"}, "I'm an h1 Tag"), 
-React.createElement("h2", {id:"h2Tag"}, "I'm an h2 Tag")]))
-const parent2 = React.createElement("div", {id: "parentDiv2"}, 
-React.createElement("div", {id:"childDiv"},
-[React.createElement("h1", {id:"h1Tag"}, "I'm an h1 Tag"), 
-React.createElement("h2", {id:"h2Tag"}, "I'm an h2 Tag")]))
-root.render([parent, parent2])
+const head = document.getElementById("root");
+const createHead = document.createElement("h2");
+console.log(head);
+console.log(createHead);
+console.log(heading);
+const parent = React.createElement(
+  "div",
+  { id: "parentDiv" },
+  React.createElement("div", { id: "childDiv1" }, [
+    React.createElement("h1", { id: "h1Tag1" }, "I'm an h1 Tag"),
+    React.createElement("h2", { id: "h2Tag1" }, "I'm an h2 Tag"),
+  ])
+);
+const parent2 = React.createElement(
+  "div",
+  { id: "parentDiv2" },
+  React.createElement("div", { id: "childDiv2" }, [
+    React.createElement("h1", { id: "h1Tag2" }, "I'm an h1 Tag"),
+    React.createElement("h2", { id: "h2Tag2" }, "I'm an h2 Tag"),
+  ])
+);
+root.render([parent, parent2]);
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Namste React
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement (Auto refreshing when saving) - Hot Reloading
+- File Watching Algorithm - Written in C++
+- Faster Builds - Due to caching(.parcel-cache)
+- Image Optimization
+- Minification of a file (For prod builds)
+- Bundling of all files
+- Compressing of files
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling (To support older browsers)
+- Good Error Hints
+- Diagnostics
+- Helps in hosting the app on HTTPs server
+- Tree Shaking - (Remove unused code from the files)
+- Different dev and prod builds
+
+# Browser List Configuration
+
+- https://browserslist.dev/?q=bGFzdCAyIHZlcnNpb25z
+- https://github.com/browserslist/browserslist#query-composition
